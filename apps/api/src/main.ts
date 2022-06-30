eecb6496b9d02336fd94e3e570920c1437c58f97
Patrick Kenyon <patrick.kenyon@playsportsnetwork.com>
2022-06-30 09:50:59 +0100

    Added API, interfaces and example sites

---
+++ apps/api/src/main.ts
@@ -0,0 +1,20 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { API_URL } from '@my-org/api-interface';
+import type { ApiResponse } from '@my-org/api-interface';
+import * as express from 'express';
+
+const app = express();
+
+app.get(API_URL, (req, res) => {
+  res.send({ message: 'Welcome to api!' } as ApiResponse);
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
